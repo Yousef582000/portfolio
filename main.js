2c8cce41baefcf13e2eadb65ddfd8bb13ecdfdb5
youseef khaled <131982745+Yousef582000@users.noreply.github.com>
2023-08-19 21:11:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let mune1 =document.getElementById('mune');
+let list1=document.getElementById('listitem');
+mune1.addEventListener('click',function(){
+
+    list1.style.display='flex';
+   mune1.style.display="none"
+
+})
